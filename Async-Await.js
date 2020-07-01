4e7efb10abb17f72d77c1c437bad836319f5d73e
Ahsan Shakeel <ahsanshakeel1994@gmail.com>
2020-07-01 17:14:56 +0500

    Update Async-Await.js

--- Async-Await.js
+++ Async-Await.js
@@ -1,9 +1,11 @@
-//1. The word “async” before a function means one simple thing: 
-//a function always returns a promise. 2. The keyword "await" makes 
-//JavaScript wait until that promise settles and returns its result.
+//	1. The word “async” before a function means one simple thing: 
+//	a function always returns a promise. 
 
+//	2. The keyword "await" makes JavaScript wait until that promise 
+//	settles and returns its result.
 
 /*********************************************************************/
+
 var promise = new Promise((resolve, reject) => { 
   let number = 2; 
   if(number === 2) { 
@@ -19,5 +21,32 @@ async function _async() {
 }
 
 _async()
+
+/*********************************************************************/
+
+const random = () => {
+	let promise = new Promise((resolve, reject) => {
+		let number = Math.floor(Math.random() * 10)
+		if (number % 2) {
+			resolve("Success")
+		} 
+		else{
+			reject("Unknown Error")
+		}
+	})
+	return promise
+}
+
+const data = async () => {
+	try{
+		let numberdata = await random()
+		console.log(numberdata)
+	}
+	catch(error){
+		console.log("Found", error)
+	}
+}
+
+data()
 
 /*********************************************************************/
